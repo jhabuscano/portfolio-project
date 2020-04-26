61a84bb9814a0849dadb98cb0c2fe20bd8532204
Jon Hermie Buscano <jhabuscano@gmail.com>
2020-04-26 08:52:51 +0800

    Homepage

--- Portfolio-project/blog/admin.py
+++ Portfolio-project/blog/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
 
-from .models import BLog
+from .models import Blog
 
-admin.site.register(BLog)
-
-
-
-
+admin.site.register(Blog)
--- Portfolio-project/blog/models.py
+++ Portfolio-project/blog/models.py
@@ -1,21 +1,11 @@
 from django.db import models
 
-# Create A Blog models
-#title
-#pub_date
-# body
-# images
 
 class Blog(models.Model):
     title = models.CharField(max_length=255)
-    pub_date = models.DateField()
+    pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
-
-
-
-
-
 
 # Add the Blog app to the setttings
 #Create a migration
